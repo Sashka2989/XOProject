3ca30609089d82c4e483177700eebac74d3d60a8
Sashka2989 <Sashu998@gmail.com>
2020-08-24 10:24:54 +0700

    Create project

---
+++ src/main/java/com/alexplot/xoapp/XoappApplication.java
@@ -0,0 +1,13 @@
+package com.alexplot.xoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexplot/xoapp/XoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexplot.xoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
